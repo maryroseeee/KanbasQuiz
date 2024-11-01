0f620aaa618b72f394caac4b3c5466bb528ceeb7
maryroseeee <91707406+maryroseeee@users.noreply.github.com>
2024-11-01 17:50:32 -0400

    Add files via upload

---
+++ kanbas-react-web-app-main/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Labs from "./Labs";
+import logo from "./logo.svg";
+// import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Web Dev</h1>
+      <Labs />
+    </div>
+  );
+}
+
+export default App;
