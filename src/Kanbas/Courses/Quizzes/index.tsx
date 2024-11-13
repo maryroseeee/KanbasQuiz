60a04227b582039f0b678ce5afed8bc197550d55
maryroseeee <91707406+maryroseeee@users.noreply.github.com>
2024-11-13 15:00:38 -0500

    quiz editor

--- src/Kanbas/Courses/Quizzes/index.tsx
+++ src/Kanbas/Courses/Quizzes/index.tsx
@@ -4,34 +4,35 @@ import QuizControlButtons from "./QuizControlButtons";
 import AssignmentPrefixButtons from "../Assignments/AssignmentPrefixButtons";
 import React  from "react";
 import { Link } from "react-router-dom";
-import { deleteAssignment}
-    from "../Assignments/reducer";
+import { deleteQuiz}
+    from "./reducer";
 import { useSelector, useDispatch } from "react-redux";
 import AssignmentIndivButtons from "../Assignments/AssignmentIndivButtons";
 import {FaPlus} from "react-icons/fa";
 import LessonControlButtons from "../Modules/LessonControlButtons";
 import * as db from "../../Database";
+import QuizIndivButtons from "./QuizIndivButtons";
 
 
 export default function Quizzes() {
     const { cid } = useParams();
-    const quizzes = db.quizzes
+    const quizzes = useSelector((state: any) => state.quizReducer.quizzes);
     // const assignments = useSelector((state: any) => state.assignmentReducer.assignments);
     const dispatch = useDispatch();
     const { currentUser } = useSelector((state: any) => state.accountReducer);
 
     return (
 
-        <li className="wd-assignment list-group-item p-0 mb-2 fs-5 border-gray">
+        <li className="wd-quiz list-group-item p-0 mb-2 fs-5 border-gray">
 
             <div className="d-flex justify-content-end mb-2">
                 {currentUser.role === "FACULTY" && ( // Only render ModulesControls if user is FACULTY
                     <Link
-                        to={`/Kanbas/Courses/${cid}/Assignments/add`}
+                        to={`/Kanbas/Courses/${cid}/Quizzes/add`}
                         className="btn btn-danger btn-lg text-decoration-none text-white"
                     >
                         <FaPlus className="me-2" />
-                        Assignment
+                        Quiz
                     </Link>
                 )}
             </div>
@@ -40,18 +41,18 @@ export default function Quizzes() {
                 <BsGripVertical className="me-2 fs-3" /> Quizzes <QuizControlButtons />
             </div>
 
-            <ul id="wd-assignments" className="list-group rounded-0">
+            <ul id="wd-quizzes" className="list-group rounded-0">
                 {quizzes
                     .filter((quiz: any) => quiz.course === cid)
                     .map((quiz: any) => (
                         <li className="wd-module list-group-item p-0 fs-5 border-gray" key={quiz.id}>
-                            <div className="wd-assignment-list-item p-3 ps-2 wd-lesson">
+                            <div className="wd-quiz-list-item p-3 ps-2 wd-lesson">
                                 <div className="d-flex justify-content-between align-items-center mb-2">
                                     <div className="d-flex align-items-center">
                                         <AssignmentPrefixButtons />
                                         {currentUser.role === "FACULTY" ? (
                                             <Link
-                                                to={`/Kanbas/Courses/${cid}/Assignments/${quiz._id}`}
+                                                to={`/Kanbas/Courses/${cid}/Quizzes/${quiz._id}`}
                                                 className="text-decoration-none text-black"
                                             >
                                                 <span className="ms-2 text-start">{quiz.title}</span>
@@ -68,9 +69,9 @@ export default function Quizzes() {
                                         //                         deleteAssignment={(assignmentId) => {
                                         //                             dispatch(deleteAssignment(assignmentId));
                                         //                         }} />
-                                        <AssignmentIndivButtons assignmentId={quiz._id}
-                                                                deleteAssignment={(assignmentId) => {
-                                                                    dispatch(deleteAssignment(assignmentId));
+                                        <QuizIndivButtons quizId={quiz._id}
+                                                          deleteQuiz={(quizId) => {
+                                                                    dispatch(deleteQuiz(quizId));
                                                                 }} />
                                     ) : (
                                         <LessonControlButtons/>
